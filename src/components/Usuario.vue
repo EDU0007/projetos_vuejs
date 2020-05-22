6582fc7f145615c652beb821552fa1f35c9f2a7a
eduardo <55513247+EDU0007@users.noreply.github.com>
2020-05-21 21:10:45 -0300

    Essa é o começo da minha jornada Vue js

---
+++ src/components/Usuario.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p><strong>{{nome}}</strong></p>
+        <p> a idade é:{{idade}}</p>
+        <button @click="alterarNome" class="btn btn-primary">Alterar nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info
+             :nome="nome"
+             :idade="idade"
+             @nomeMudou="nome = $event "
+             :reiniciarFn="reiniciarNome"
+             />
+            <app-usuario-editar
+            :idade="idade"/>
+           
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+     data() {
+         return {
+             nome: "Eduardo",
+             idade: 23
+            
+         }
+     },
+     methods: {
+        
+         alterarNome(){
+             this.nome = 'Eduardo santana'
+         },
+
+         reiniciarNome(){
+             this.nome = "santana santos"
+         }
+     },
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>idade:{{idade}}</p>
+        <p><strong>{{inverterr()}}</strong></p>
+        <button @click="reiniciarNome">Reiniciar</button>
+        <button @click="reiniciarFn()">Reiniciar(callback)</button>
+    </div>
+</template>
+
+<script>
+import barramento from '@/barramento'
+export default {
+    props:{
+        nome: {
+            type: String,
+             default: "anomino"
+        },
+        idade: Number,
+       reiniciarFn: Function
+
+    },
+
+    methods: {
+        inverterr(){
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            this.nome = 'pedro'
+            this.$emit("nomeMudou",this.nome)
+        }
+    },
+
+    created() {
+            barramento.quandoIdadeMudar(idade =>{
+                this.idade = idade
+            })
+    }
+
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
